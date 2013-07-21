294890191b5860afd4c9afc18723550d457600f9
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-07-22 00:10:29 +0900

    update anything

--- len.rb
+++ len.rb
@@ -1,11 +1,13 @@
 # coding:utf-8
 class Len
   attr_reader :size
+
   def initialize(str)
     @str = str.to_s
     @size = 0
     get_sizes
   end
+
   def get_sizes
     @str.scan(/./) do |i|
       if(/[ -~｡-ﾟ]/ =~ i) # 半角
