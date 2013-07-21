595fbb65a1163beadd284860aa1ceefa0b6b0c26
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2013-07-22 00:08:53 +0900

    update anything

--- len.rb
+++ len.rb
@@ -1,3 +1,18 @@
 # coding:utf-8
 class Len
+  attr_reader :size
+  def initialize(str)
+    @str = str.to_s
+    @size = 0
+    get_sizes
+  end
+  def get_sizes
+    @str.scan(/./) do |i|
+      if(/[ -~｡-ﾟ]/ =~ i) # 半角
+        @size += 1
+      else
+        @size += 2
+      end
+    end
+  end
 end
--- len_spec.rb
+++ len_spec.rb
@@ -2,4 +2,45 @@
 require "./len"
 
 describe Len do
+  it 'should empty string' do
+    str = Len.new('')
+    str.size.should == 0
+  end
+  it 'should ascii string' do
+    str = Len.new('AAAAA')
+    str.size.should == 5
+  end
+  it 'should KANJI string' do
+    str = Len.new('うへ')
+    str.size.should == 4
+  end
+  it 'should mix string' do
+    str = Len.new('うaへkk')
+    str.size.should == 7
+  end
+  it 'should mix string' do
+    str = Len.new('kうaへ')
+    str.size.should == 6
+  end
+  it 'shuld numeric' do
+    str = Len.new(3)
+    str.size.should == 1
+  end
+  it 'shuld empty array 2' do
+    str = Len.new([])
+    str.size.should == 2
+  end
+  it 'shuld array 9' do
+    str = Len.new([a, b, c])
+    str.size.should == 9
+  end
+  it 'shuld empty hash 2' do
+    str = Len.new({})
+    str.size.should == 2
+  end
+  it 'shuld empty hash 26' do
+    str = Len.new({"test" => 3, 5 => "test"})
+    str.size.should == 26
+  end
+
 end
